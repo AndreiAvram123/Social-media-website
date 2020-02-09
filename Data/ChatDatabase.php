14003c5ef72215a5ad9c90bdc7d88c5c78772de2
avram <avramandreitiberiu@gmail.com>
2020-02-09 14:56:54 +0000

    -notification sound added for message system -messaging system working
    KNOWN BUGS :
    - the message container overflows on the x axis
    - sometimes the messaging system fetches the same message twice

--- Data/ChatDatabase.php
+++ Data/ChatDatabase.php
@@ -41,6 +41,7 @@ class ChatDatabase
     public function insertNewMessage($messageContent, $date, $sender_id, $receiver_id)
     {
         $query = "INSERT INTO messages VALUES (NULL,'$messageContent','$date', '$sender_id','$receiver_id')";
+
         $result = $this->_dbHandler->prepare($query);
         $result->execute();
 
@@ -59,5 +60,50 @@ class ChatDatabase
         }
         return $messages;
     }
+
+    public function createChat($user1Id, $user2Id)
+    {
+        $query = "INSERT INTO chats values (NULL,$user1Id,$user2Id)";
+        $this->executeQuery($query);
+    }
+
+    public function createChatLiveFunctions($charId, $user1Id, $user2Id)
+    {
+        $query = "INSERT INTO chat_live_functions VALUES ('$charId','$user1Id',false)";
+        $secondQuery = "INSERT INTO chat_live_functions VALUES ('$charId','$user2Id',false)";
+        $this->executeQuery($query);
+        $this->executeQuery($secondQuery);
+    }
+
+    private function executeQuery($query)
+    {
+        $result = $this->_dbHandler->prepare($query);
+        $result->execute();
+    }
+
+    public function fetchChatId($user1Id, $user2Id)
+    {
+        $query = "SELECT chat_id from chats WHERE (user1_id = '$user1Id' AND user2_id = '$user2Id')
+OR (user1_id = '$user2Id' AND user2_id='$user1Id')";
+        $result = $this->_dbHandler->prepare($query);
+        $result->execute();
+        $row = $result->fetch();
+        return $row['chat_id'];
+    }
+
+    public function setUserIsTyping($chatId, $userId, $isTyping)
+    {
+        $query = "UPDATE chat_live_functions SET user_is_typing = $isTyping  WHERE chat_id='$chatId' AND user_id = '$userId'";
+        $this->executeQuery($query);
+    }
+
+    public function checkUserIsTyping($chatId,$currentUserId)
+    {
+      $query = "SELECT user_is_typing FROM chat_live_functions WHERE chat_id = '$chatId' AND user_id != '$currentUserId'";
+        $result = $this->_dbHandler->prepare($query);
+        $result->execute();
+        return ($result->fetch())["user_is_typing"];
+    }
+
 
 }
--- Javascript/ChatManager.js
+++ Javascript/ChatManager.js
@@ -1,23 +1,55 @@
 //check the database every third of  a second
 let timeIntervalCheck = 300;
+let timeIntervalCheckTyping = 700;
 let intervalCheck;
+let userIsTypingCheck;
+let currentUserIsTypingCheck;
 let lastMessageId;
 let sessionUserId;
 let chatWindowInitialized = false;
+let chatId;
+let lastKeyPressedTime;
+let shouldFetchNewMessages;
+let userIsTypingHint;
+
+function checkUserIsTyping() {
+    let url = "ChatController.php?requestName=checkUserIsTyping";
+    url += "&userId=" + sessionUserId;
+    url += "&chatId=" + chatId;
+    getXmlHttpGetRequest(url).onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+            if (this.responseText == 1) {
+                displayUserTypingHint(true);
+            } else {
+                displayUserTypingHint(false)
+            }
+        }
+
+    };
+}
+
+function displayUserTypingHint(isTyping) {
+    if (isTyping === true) {
+        userIsTypingHint.style.display = "block";
+    } else {
+        userIsTypingHint.style.display = "none";
+    }
+}
 
 function fetchNewMessages(receiverId, container) {
-
     let url = "ChatController.php?requestName=fetchNewMessages";
     url += "&lastMessageId=" + lastMessageId;
     url += "&currentUserId=" + sessionUserId;
     url += "&receiverId=" + receiverId;
-
-    if (lastMessageId != null) {
+    if (lastMessageId != null && shouldFetchNewMessages === true) {
+        shouldFetchNewMessages = false;
         getXmlHttpGetRequest(url).onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
                 if (this.responseText !== "No results") {
                     processMessages(this.responseText, container);
                 }
+                shouldFetchNewMessages = true;
             }
         };
     }
@@ -26,25 +58,21 @@ function fetchNewMessages(receiverId, container) {
 function sendMessage(receiverId) {
     let messageField = document.getElementById("messageField");
     let message = messageField.value.trim();
-    //  let messageContainer = document.getElementsByClassName("message-container")[0];
     if (message !== "") {
-
         let dataToSend = "messageContent=" + message;
         dataToSend += "&receiverId=" + receiverId;
         dataToSend += "&currentUserId=" + sessionUserId;
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.open("POST", "ChatController.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(dataToSend);
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                console.log(this.responseText);
-            }
-
-        };
+        getXmlHttpPostRequest(dataToSend);
         messageField.value = "";
     }
+}
+
+function getXmlHttpPostRequest(dataToSend) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "ChatController.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(dataToSend);
+    return xhttp;
 }
 
 function getXmlHttpGetRequest(url) {
@@ -53,6 +81,7 @@ function getXmlHttpGetRequest(url) {
     xhttp.send();
     return xhttp;
 }
+
 
 function startChat(currentUserId, receiverId, username) {
     if (chatWindowInitialized === false) {
@@ -66,21 +95,39 @@ function startChat(currentUserId, receiverId, username) {
             '        <div class ="message-container">\n' +
             '\n' +
             '        </div>\n' +
+            '<p id="user-is-typing-hint">User is typing</p>' +
             '        <div class="message-footer">\n' +
-            '            <textarea type="text" id="messageField" ></textarea>\n' +
+            '            <textarea type="text" id="messageField"></textarea>\n' +
             '            <button onclick="sendMessage(' + '\'' + receiverId + '\'' + ')"><i\n' +
             '                    class="far fa-paper-plane"></i></button>\n' +
             '        </div>\n' +
             '    </div>\n'
         '</div>';
-
-        (document.getElementById("messageField")).addEventListener('keypress', (event) => {
-            if (event.keyCode === 13) {
+        userIsTypingHint = document.getElementById("user-is-typing-hint");
+        userIsTypingHint.style.display = "none";
+        (document.getElementById("messageField")).addEventListener('keyup', (event) => {
+            if (event.key === "Enter") {
                 sendMessage(receiverId);
+            } else {
+                if (event.key !== "Backspace") {
+                    markCurrentUserAsTyping(true);
+                }
             }
+
+
         });
         fetchChatMessages(receiverId, document.getElementsByClassName("message-container")[0]);
     }
+
+}
+
+function markCurrentUserAsTyping($isTyping) {
+    lastKeyPressedTime = new Date().getTime();
+    let dataToSend = "ChatController.php?requestName=markTyping";
+    dataToSend += "&userId=" + sessionUserId;
+    dataToSend += "&chatId=" + chatId;
+    dataToSend += "&isTyping=" + $isTyping;
+    getXmlHttpGetRequest(dataToSend);
 }
 
 function removeChat(chat) {
@@ -131,17 +178,46 @@ function scrollToLastMessage(container) {
     container.scrollTop = container.lastChild.offsetTop;
 }
 
+function checkCurrentUserTyping() {
+    let currentTime = new Date().getTime();
+    if (lastKeyPressedTime + 20 < currentTime) {
+        markCurrentUserAsTyping(false);
+    }
+}
 
 function fetchChatMessages(user2Id, container) {
+
+    function getChatId() {
+        let url = "ChatController.php?requestName=fetchChatId";
+        url += "&user1Id=" + sessionUserId;
+        url += "&user2Id=" + user2Id;
+        getXmlHttpGetRequest(url).onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                chatId = this.responseText;
+            }
+        }
+    }
+
+    getChatId();
     let url = "ChatController.php?requestName=fetchMessages";
     url += "&user1Id=" + sessionUserId;
     url += "&user2Id=" + user2Id;
+
+    function initializeAsyncFunctions() {
+        intervalCheck = setInterval(fetchNewMessages, timeIntervalCheck, user2Id, container);
+        userIsTypingCheck = setInterval(checkUserIsTyping, timeIntervalCheckTyping);
+        currentUserIsTypingCheck = setInterval(checkCurrentUserTyping, timeIntervalCheckTyping);
+
+    }
+
     getXmlHttpGetRequest(url).onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             if (this.responseText.trim() !== "No results") {
                 processMessages(this.responseText, container, sessionUserId);
-                intervalCheck = setInterval(fetchNewMessages, timeIntervalCheck, user2Id, container);
+                initializeAsyncFunctions();
+
             }
+            shouldFetchNewMessages = true;
         }
     };
 }
