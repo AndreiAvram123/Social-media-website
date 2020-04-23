b515dfeeb941b2b31d93d62dbfe3ac9928246051
AndreiAvram123 <avramandreitiberiu@gmail.com>
2020-04-23 23:22:40 +0100

    -added javascript validation for add post

--- Javascript/AddPost.js
+++ Javascript/AddPost.js
@@ -1,33 +1,61 @@
 function submitPost(userID) {
 
-    let postTitle = document.getElementById("postTitle").value;
-    let postCategory = document.getElementById("postCategorySelector").value;
-    let postContent = document.getElementById("postContent").value;
+
+    let postTitle = document.getElementById("postTitle");
+    let postCategory = document.getElementById("postCategorySelector");
+    let postContent = document.getElementById("postContent");
     let file = document.getElementById("fileToUpload").files[0];
+    let errorElement = document.getElementById("errorAddPost");
+    clearFields();
 
-    function displayError() {
+    function clearFields() {
+        postTitle.className = "form-control";
+        postContent.className = "form-control text-area";
+    }
 
+    function displayError(error) {
+        errorElement.style.visibility = "visible";
+        errorElement.innerText = error;
     }
 
     if (arePostsDetailsValid()) {
         pushRequest().then(() => {
             location.reload();
         }).catch(error => {
-            displayError();
+            displayError(error);
         })
     }
 
     function arePostsDetailsValid() {
+        if (postTitle.value.trim() === "") {
+            displayError("The title should not be empty");
+            highlightElement(postTitle);
+            return false;
+        }
+
+        if (postContent.value.trim() === "") {
+            displayError("You must enter some text for your post");
+            highlightElement(postContent);
+            return false;
+        }
+        if (file === undefined) {
+            displayError("You must select an image");
+            return false;
+        }
         return true;
+    }
+
+    function highlightElement(element) {
+        element.className += " error-field";
     }
 
     function pushRequest() {
         return new Promise((resolve, reject) => {
             let url = "AddPost.php?" + "apiKey=" + apiKey;
             let formData = new FormData();
-            formData.append("postTitle", postTitle);
-            formData.append("postCategory", postCategory);
-            formData.append("postContent", postContent);
+            formData.append("postTitle", postTitle.value);
+            formData.append("postCategory", postCategory.value);
+            formData.append("postContent", postContent.value);
             formData.append("imageName", file.name);
             formData.append("userID", userID);
             Promise.all([resizeImage(file, 600, 500),
